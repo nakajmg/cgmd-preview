37ee5d0d7fea00e31d9afa7916131da3d00c776b
nakajmg <nakajima.jmg@gmail.com>
2016-06-24 00:27:58 +0900

    ビルドタスク追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const watchify = require('gulp-watchify');
+const sequence = require('run-sequence');
 
 const main = './index.js';
+const browser = './browser.js';
 
 gulp.task('browserify', watchify((_watchify) => {
   var stream = gulp.src(main)
@@ -28,3 +30,39 @@ gulp.task('browserify', watchify((_watchify) => {
 
   return stream;
 }));
+
+gulp.task('browser', watchify((_watchify) => {
+  var stream = gulp.src(browser)
+    .pipe(_watchify({
+      ignoreMissing: true,
+      detectGlobals: false,
+      watch: false,
+      transform: [
+        ['babelify', {
+          presets: [
+            'es2015',
+            'stage-3'
+          ],
+          plugins: [
+            'transform-async-to-generator',
+            'syntax-async-functions',
+            'transform-regenerator'
+          ]
+        }]
+      ],
+      builtins: [],
+    }))
+    .pipe(gulp.dest('dist'));
+
+  return stream;
+}));
+
+gulp.task('copy', (cb) => {
+  return gulp.src(['style.min.css', 'window.html'])
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', () => {
+  return sequence(['browserify', 'browser'], 'copy');
+});
+
